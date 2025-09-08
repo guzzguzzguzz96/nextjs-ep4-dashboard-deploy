85dfc811e195d1460cc5c4e1dcdf2e4bd6917c92
guzzguzzguzz96 <guzzguzzguzz96@gmail.com>
2025-09-08 10:54:37 +0700

    first commit

---
+++ src/app/admin/components/Content.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaUsers, FaRegNewspaper } from "react-icons/fa6";
+
+function Content({ totalUsersData, totalPostsData }) {
+  return (
+    <div className="px-10 rounded-lg">
+      <div className="flex">
+        <div className="shadow-lg w-[300px] m-3 p-10 rounded-lg">
+          <h3 className="flex items-center">
+            <FaUsers className="mr-2" />
+            Total Users
+          </h3>
+          <p className="text-5xl mt-10">{totalUsersData?.length}</p>
+        </div>
+        <div className="shadow-lg w-[300px] m-3 p-10 rounded-lg">
+          <h3 className="flex items-center">
+            <FaRegNewspaper className="mr-2" />
+            Total Posts
+          </h3>
+          <p className="text-5xl mt-10">{totalPostsData?.length}</p>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, aliquid, atque cumque blanditiis eaque eum quam unde, praesentium vitae porro quaerat? Veritatis deserunt placeat, totam eius fugit harum est eaque?
+      </p>
+    </div>
+  );
+}
+
+export default Content;
